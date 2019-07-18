9b2c2fdfae0422f40507f3ef190bea008c96fca0
ivajo26 <ivajodimo26@gmail.com>
2019-07-18 14:53:02 -0500

    Clase 9

--- index.js
+++ index.js
@@ -1,31 +1,15 @@
-// Ejemplo 1
-
-let var1 = 20
-let var2 = 40
-
-if (var1 === var2) {
-    console.log("Son iguales los valores.")
-} else if (var1 > var2) {
-    console.log("La variable 1 es mayor que la variable 2")
-} else {
-    console.log("Los valores no son iguales.")
+for(let i = 20; i >= 10; i--) {
+    console.log(i)
 }
 
-// Ejemplo 2
+let j = 0
 
-let var1 = 20
-let var2 = 40
-
-let result = (var1 === var2) ? "Los valores son iguales" : "No son iguales los valores."
-
-console.log(result)
-
-// Ejemplo 3
-let var1 = 20
-
-let min = 10 
-let max = 40
-
-if ((var1 > min) && (var1 < max)) {
-    console.log("Var1 esta en el rango")
+while(j <= 3) {
+    console.log(j);
+    j += 4
 }
+
+do {
+    console.log(j);
+    j += 4
+} while(j <= 3)
