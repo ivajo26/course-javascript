07830165185c8c1e1d9d2adc6f2c07de00785d70
ivajo26 <ivajodimo26@gmail.com>
2019-07-19 11:02:26 -0500

    Clase 11

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 function residuo() {
     // console.log("El residuo de 11 / 2 = ", 11 % 2)
-    return 11 % 2
+    function text() {
+        return "El residuo es = "
+    }
+    let value = 11 % 2
+    return value
 }
 
 let residuo2 = function() {
     return 11 % 2
 }
 
+
 for(let i = 0; i <= 10; i++) {
-    let result = residuo2()
-    console.log("El residuo de 11 / 2 = ", result)
+    let result = residuo()
+    console.log(text(), result)
 }
