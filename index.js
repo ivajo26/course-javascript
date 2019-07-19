040047f2315884676edba652adac40225a96a3ae
ivajo26 <ivajodimo26@gmail.com>
2019-07-19 16:33:24 -0500

    Clase 16

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 let array1 = [30,60,70,20,45]
 
-let suma = 0
-for(let i = 0; i < array1.length; i++) {
-    // console.log(`La posicion ${i} tiene el valor ${array1[i]}`)
-    suma += array1[i]
-}
+let array2 = array1.map(item => item * 2)
+console.log("El array2: ", array2)
 
-console.log(`El valor total de la suma es ${suma}`)
+let array3 = array1.filter(item => item % 2 === 0)
+console.log("El array3: ", array3)
+
+let array4 = array1.find(item => item === 70)
+console.log("El array4: ", array4)
+
+let array5 = array1.shift()
+console.log("El array5: ", array5)
+console.log("El array1: ", array1)
