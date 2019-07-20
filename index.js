da8652aac98e4222c21948d50a5ba967a497988b
ivajo26 <ivajodimo26@gmail.com>
2019-07-20 09:46:24 -0500

    Clase 17

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-let array1 = [30,60,70,20,45]
+let var1 = {vagon1: 90, gato1: 67}
+let key1 = 'vagon1'
+console.log(var1[key1])
 
-let array2 = array1.map(item => item * 2)
-console.log("El array2: ", array2)
+let students = [
+    {name: "Jaimito", apellido: "Jalal", points: [4.5, 5, 3.2]},
+    {name: "Pepita", apellido: "Perez", points: [1.5, 2, 2.2]},
+    true,
+    50,
+    "Hola Mundo"
+]
 
-let array3 = array1.filter(item => item % 2 === 0)
-console.log("El array3: ", array3)
-
-let array4 = array1.find(item => item === 70)
-console.log("El array4: ", array4)
-
-let array5 = array1.shift()
-console.log("El array5: ", array5)
-console.log("El array1: ", array1)
+console.log(students)
