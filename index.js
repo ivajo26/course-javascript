b3aa18e99a7b3a874b431a51b63fd6997460569a
ivajo26 <ivajodimo26@gmail.com>
2019-07-20 11:33:15 -0500

    Clase 25

--- index.js
+++ index.js
@@ -7,11 +7,18 @@ function function1(message, time) {
 
 }
 
-let todos = Promise.all([
-    function1("Function 1", 2000),
-    function1("Function 2", 1000),
-    function1("Function 3", 5000),
-    function1("Function 4", 3000)
-])
+async function ejecutando() {
+    let [fun1, fun2, fun3, fun4] = await Promise.all([
+        function1("Function 1", 2000),
+        function1("Function 2", 1000),
+        function1("Function 3", 5000),
+        function1("Function 4", 3000)
+    ])
+    
+    console.log(fun1)
+    console.log(fun2)
+    console.log(fun3)
+    console.log(fun4)
+}
 
-console.log(todos)
+ejecutando()
