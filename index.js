141bc8cc8309928acd6b5ebb6a08b7c5b40e44bb
ivajo26 <ivajodimo26@gmail.com>
2019-07-20 10:11:17 -0500

    Clase 19

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 let var1 = {vagon1: 90, gato1: 67}
-let key1 = 'vagon1'
-console.log(JSON.stringify(var1))
 
 let students = [
     {name: "Jaimito", apellido: "Jalal", points: [4.5, 5, 3.2]},
     {name: "Pepita", apellido: "Perez", points: [1.5, 2, 2.2]},
-    true,
-    50,
-    "Hola Mundo"
 ]
 
-let json = JSON.stringify(students)
+Object.keys(var1).map(key => console.log(var1[key]))
 
-console.log(json)
+console.log(Object.entries(var1))
 
-console.log(JSON.parse(json))
+let array1 = [1,4,5,2]
+
+for(let [key, value] of Object.entries(var1)) {
+    console.log(`${key} - ${value}`)
+}
+
+let obj1 = {a:2, b:4,c:6}
+let obj2 = {z:2, x:4,w:6}
+
+let obj3 = Object.assign(obj1, obj2)
+
+console.log(obj3)
