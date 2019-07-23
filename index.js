34dfe2df518e7500510a02c56453e50bf9895c2a
ivajo26 <ivajodimo26@gmail.com>
2019-07-23 17:02:58 -0500

    Clase 28

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-import function1 from 'funciones.js'
-const function1 = require('funciones.js')
-async function ejecutando() {
-    let [fun1, fun2, fun3, fun4] = await Promise.all([
-        function1("Function 1", 2000),
-        function1("Function 2", 1000),
-        function1("Function 3", 5000),
-        function1("Function 4", 3000)
-    ])
-    
-    console.log(fun1)
-    console.log(fun2)
-    console.log(fun3)
-    console.log(fun4)
-}
+const titulo = document.getElementsByClassName('spanClass')
+const parrafo = document.getElementById('id1')
 
-ejecutando()
+console.log(titulo)
+console.log(parrafo)
